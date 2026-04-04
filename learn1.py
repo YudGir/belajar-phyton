6f6f258b4756fd7d280274ca41417cf75d21d4dd
YudGir <yudhagirsang07@gmail.com>
2026-04-04 18:09:12 +0700

    menambahkan lanjutan tipe data

--- learn1.py
+++ learn1.py
@@ -5,3 +5,17 @@ print(angka_bulat)
 
 print(type(""))
 print(round(10.18924912, 1))
+
+is_true = (5 < 3)
+print (is_true)
+false = not is_true
+print(false)
+
+new_string = "abc" + "def"
+print(new_string)
+
+newer_string = "abc" + True
+print(newer_string)
+
+newest_string = ("abc" + " ")* 5
+print(newest_string)
